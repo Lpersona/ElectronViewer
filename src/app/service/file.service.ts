685304e667a9cfb390a6c60d12c386a7fafd366e
Liusiyan <liusiyan92@qq.com>
2019-11-06 19:30:48 +0800

    修改添加tileset逻辑

--- src/app/service/file.service.ts
+++ src/app/service/file.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IpcRenderer } from 'electron';
-import { ViewerService } from './viewer.service';
+import { TilesetService } from './tileset.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,7 @@ import { ViewerService } from './viewer.service';
 export class FileService {
   private ipc: IpcRenderer;
 
-  constructor(private viewerService: ViewerService) {
+  constructor(private tilesetService: TilesetService) {
     if ((window as any).require) {
       this.ipc = (window as any).require('electron').ipcRenderer;
     }
@@ -17,32 +17,7 @@ export class FileService {
   public getFile() {
     this.ipc.send('getFiles');
     this.ipc.once('getFileResponse', (event: any, url, port) => {
-      console.log(event);
-      console.log(port);
-      console.log(url);
-
-      const viewer = this.viewerService.getViewer();
-      const tileset = viewer.scene.primitives.add(
-        new Cesium.Cesium3DTileset({
-          url,
-          skipLevelOfDetail: true,
-          maximumNumberOfLoadedTiles: 500,
-          maximumMemoryUsage: 512,
-          baseScreenSpaceError: 1024,
-          skipScreenSpaceErrorFactor: 16,
-          skipLevels: 1,
-          immediatelyLoadDesiredLevelOfDetail: false,
-          loadSiblings: false,
-          maximumScreenSpaceError: 1,
-          cullWithChildrenBounds: true,
-          // debugShowBoundingVolume: true
-          refineToVisible: false
-        })
-      );
-
-      tileset.readyPromise.then((tile: any) => {
-        viewer.camera.flyToBoundingSphere(tile.boundingSphere);
-      });
+      this.tilesetService.loadTileset(url, port, name);
     });
   }
 }
--- src/app/service/tileset.service.ts
+++ src/app/service/tileset.service.ts
@@ -1,17 +1,71 @@
 import { Injectable } from '@angular/core';
+import { ViewerService } from './viewer.service';
+import { IpcRenderer } from 'electron';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TilesetService {
   public tilelist: any[];
-  constructor() {}
+  public viewer: any;
+  private ipc: IpcRenderer;
 
-  public loadTileset(url: string) {
-    this.tilelist.push();
+  constructor(private viewerService: ViewerService) {
+    if ((window as any).require) {
+      this.ipc = (window as any).require('electron').ipcRender;
+    }
+
+    this.tilelist = [];
   }
 
-  public removeTileset(tileset: any) {}
+  public loadTileset(url: string, portNumber: number, name: string) {
+    if (!this.viewer) {
+      this.viewer = this.viewerService.getViewer();
+    }
+    const tileset = this.viewer.scene.primitives.add(
+      new Cesium.Cesium3DTileset({
+        url,
+        skipLevelOfDetail: true,
+        maximumNumberOfLoadedTiles: 500,
+        maximumMemoryUsage: 512,
+        baseScreenSpaceError: 1024,
+        skipScreenSpaceErrorFactor: 16,
+        skipLevels: 1,
+        immediatelyLoadDesiredLevelOfDetail: false,
+        loadSiblings: false,
+        maximumScreenSpaceError: 1,
+        cullWithChildrenBounds: true,
+        refineToVisible: false
+      })
+    );
 
-  public toggleTileset(tileset: any) {}
+    tileset.readyPromise.then((tile: any) => {
+      this.viewer.camera.flyToBoundingSphere(tile.boundingSphere);
+
+      this.tilelist.push({
+        tile,
+        portNumber,
+        name
+      });
+    });
+  }
+
+  public removeTileset(tileset: any) {
+    if (!this.viewer) {
+      this.viewer = this.viewerService.getViewer();
+    }
+
+    if (tileset) {
+      const { tile, portNumber } = tileset;
+      this.viewer.scene.primitives.remove(tile);
+      this.ipc.send('closePort', portNumber);
+    }
+  }
+
+  public toggleTileset(tileset: any) {
+    if (tileset) {
+      const { tile } = tileset;
+      tile.show = !tile.show;
+    }
+  }
 }
