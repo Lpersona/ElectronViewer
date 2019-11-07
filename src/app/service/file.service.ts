ab2f746eedeb7baee5ca40ce0a38bcd472f04c2e
Liusiyan <liusiyan92@qq.com>
2019-11-07 20:53:03 +0800

    添加文件加载前的命名弹窗

--- src/app/service/file.service.ts
+++ src/app/service/file.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IpcRenderer } from 'electron';
 import { TilesetService } from './tileset.service';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from '../components/dialog/dialog.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,10 @@ import { TilesetService } from './tileset.service';
 export class FileService {
   private ipc: IpcRenderer;
 
-  constructor(private tilesetService: TilesetService) {
+  constructor(
+    private tilesetService: TilesetService,
+    private dialog: MatDialog
+  ) {
     if ((window as any).require) {
       this.ipc = (window as any).require('electron').ipcRenderer;
     }
@@ -16,8 +21,14 @@ export class FileService {
 
   public getFile() {
     this.ipc.send('getFiles');
-    this.ipc.once('getFileResponse', (event: any, url, port) => {
-      this.tilesetService.loadTileset(url, port, name);
+    this.ipc.once('getFileResponse', (event: any, url, portNumber) => {
+      const dialogRef = this.dialog.open(DialogComponent, {
+        width: '250px',
+        data: { info: '请输入场景名称' }
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        this.tilesetService.loadTileset(url, portNumber, result);
+      });
     });
   }
 }
