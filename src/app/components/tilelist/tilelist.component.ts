9ab95c7b45c8809e59e4e2f5b781c7534efa7c47
Liusiyan <liusiyan92@qq.com>
2019-11-07 20:51:51 +0800

    修改列表组件加载,添加飞往功能

--- src/app/components/tilelist/tilelist.component.ts
+++ src/app/components/tilelist/tilelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TilesetService } from 'src/app/service/tileset.service';
 
 @Component({
   selector: 'app-tilelist',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tilelist.component.scss']
 })
 export class TilelistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public list: any[];
+  constructor(private tilesetService: TilesetService) {
+    this.list = tilesetService.tilelist;
   }
 
+  ngOnInit() {}
+
+  public deleteTile(tileset: any) {
+    this.tilesetService.removeTileset(tileset);
+  }
+
+  public toggleTile(tileset: any) {
+    this.tilesetService.toggleTileset(tileset);
+  }
+
+  public flyToTile(tilelist: any) {
+    this.tilesetService.flyToTileset(tilelist);
+  }
 }
